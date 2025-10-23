7c82988faf53f981c96901f37f427b1430b1a4e8
Romulo Rosa | Front-End <romulorosafullstack@gmail.com>
2025-10-23 15:48:29 -0300

    desafio layout

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,70 @@
-import "./reset.css"
+import "./index.css"
+import "../App.css"
+import logoImg from "./assets/logo.png"
+
 
 export default function App() {
   return (
     <>
-      <h1>Comparador</h1>
+      <main className="main__wrapper">
+        <div className="main__container">
+          <img
+            src={logoImg}
+            alt="logo do comparador de preços"
+            className="logo"
+          />
+
+          <h1 className="title">Comparador de Preços</h1>
+
+          <form action="" className="form__wrapper">
+            <div className="form__container">
+              <div className="form__field">
+                <label
+                  htmlFor="gasolina"
+                  className="form__label"
+                >
+                  Álcool (preço por litro):
+                </label>
+                <input
+                  type="number"
+                  id="alcool"
+                  name="alcool"
+                  required
+                  className="form__input"
+                  placeholder="Informe o preço do álcool"
+                  min="0.00"
+                  step="0.01"
+                />
+              </div>
+
+              <div className="form__field">
+                <label
+                  htmlFor="gasolina"
+                  className="form__label"
+                >
+                  Gasolina (preço por litro):
+                </label>
+                <input
+                  type="number"
+                  id="gasolina"
+                  name="gasolina"
+                  required
+                  className="form__input"
+                  placeholder="Informe o preço da gasolina"
+                  min="0.00"
+                  step="0.01"
+                />
+              </div>
+
+              <input
+                type="submit"
+                value="Comparar"
+                className="form__submit"
+              />
+            </div>
+          </form>
+        </div>
+      </main>
     </>
   )
 }
